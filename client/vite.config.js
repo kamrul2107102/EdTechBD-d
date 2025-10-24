a3afefcfe590c1e5b66289f5ac0e0163822803ca
kislam102-ux <islam2107102@stud.kuet.ac.bd>
2025-10-24 19:42:17 +0600

    Update global styles

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,35 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'react-syntax-highlighter',
+      'react-syntax-highlighter/dist/esm/languages/hljs/javascript',
+      'react-syntax-highlighter/dist/esm/languages/hljs/python',
+      'react-syntax-highlighter/dist/esm/styles/hljs'
+    ],
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'ignore-refractor',
+          setup(build) {
+            // Mark refractor imports as external - both lang and lib
+            build.onResolve({ filter: /^refractor\/(lang|lib)\// }, () => ({
+              path: 'refractor-stub',
+              namespace: 'refractor-stub'
+            }))
+            build.onLoad({ filter: /.*/, namespace: 'refractor-stub' }, () => ({
+              contents: 'export const refractor = {}; export default {};',
+              loader: 'js'
+            }))
+          }
+        }
+      ]
+    }
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-syntax-highlighter/, /node_modules/]
+    }
+  }
 })
